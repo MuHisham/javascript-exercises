1768ce867f37da3e2731a76fce1d06effbe37621
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-19 16:59:57 +0400

    Solved exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...moreArgs) {
+    let result = arr.slice();
+
+    for (let i = 0; i < moreArgs.length; i++) {
+        if (result.includes(moreArgs[i])) {
+            while (result.includes(moreArgs[i])) {
+                result.splice(result.indexOf(moreArgs[i]), 1);
+            };
+        };
+    };
+
+    return result;
 };
 
 // Do not edit below this line
