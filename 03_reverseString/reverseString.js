8ce3544428329ee63da5ed835976c4544f6be772
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-19 15:29:09 +0400

    Solved third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let charLst = phrase.split('');
+    let reverseLst = [];
+    for (let i = 0; i < charLst.length; i++) {
+        reverseLst.push(charLst[charLst.length - 1 - i]);
+    }
+    return reverseLst.join('');
 };
 
 // Do not edit below this line
