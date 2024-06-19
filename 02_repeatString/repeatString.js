e2dddc10735b1c0ba621f6f22cde4f53c45002e3
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-19 13:35:44 +0400

    Solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, noOfRepeat) {
+    let resultString = ''; 
+    if (noOfRepeat > 0) {
+        for (let i = 0; i < noOfRepeat; i++) {
+            resultString += word;
+        }
+    }
+    else if (noOfRepeat === 0) {
+        resultString = '';
+    }
+    else {
+        return 'ERROR';
+    }
+    return resultString;
 };
 
 // Do not edit below this line
