6f77cd4d6d10ab30cb7e27167abd2a6ce83e62a2
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-20 14:39:09 +0400

    Solved 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let oldestPerson = people.reduce((greatestPerson, person) => {
+        if (!person.hasOwnProperty('yearOfDeath')) person.yearOfDeath = new Date().getFullYear();
+        if (!greatestPerson.hasOwnProperty('yearOfDeath')) greatestPerson.yearOfDeath = new Date().getFullYear();
+        let currentAge = person.yearOfDeath - person.yearOfBirth;
+        let greatestAge = greatestPerson.yearOfDeath - greatestPerson.yearOfBirth;
+        if (currentAge > greatestAge) greatestPerson = person;
+        return greatestPerson;
+    })
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
