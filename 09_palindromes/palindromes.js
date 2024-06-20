808caf00aa6e77657306325c1e0269b10191bc16
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-20 13:46:39 +0400

    Solved 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let lst = phrase.toLowerCase().split('');
+    let isAlpha = (char) => ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9'));
+    let newLst = lst.filter(isAlpha);
+    return (newLst.join('') === newLst.reverse().join('')) ? true : false;
 };
 
 // Do not edit below this line
