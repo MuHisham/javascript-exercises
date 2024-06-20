b2fa18b77d5ac8341ef59231c204e77d5fe27a41
MuHisham <172366361+MuHisham@users.noreply.github.com>
2024-06-20 12:34:47 +0400

    Solved 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let result = 0;
+    if ((a >= 0 && a === parseInt(a)) && (b >= 0 && b === parseInt(b))) {
+        for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            result += i;
+        };
+        return result;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
